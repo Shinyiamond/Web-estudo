3ef49a329f54f4783c51df67d634e607da98e512
ShinyDiamond <a.icaro@escola.pr.gov.br>
2025-08-12 19:58:38 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -2,4 +2,6 @@
 const perguntas = [
   { pergunta: "O que é HTML?", resposta: "Linguagem de marcação para estruturar páginas web." },
   { pergunta: "O que é CSS?", resposta: "Linguagem de estilo para páginas web." },
+  { pergunta: "O que é JS?", resposta: é uma linguagem de programação de alto nível, amplamente utilizada para criar páginas web interativas e dinâmicas" },
 ];
+
